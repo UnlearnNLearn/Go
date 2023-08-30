940382d7483d5a79269965077a33ada870df2faf
Unlearn N Learn <avinil.bedarkar10@gmail.com>
2023-08-30 14:12:27 +0530

    video 10 and renaming directories

---
+++ call-math/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"mth"
+)
+
+func main() {
+	fmt.Println("adding 5,4 : addition is :", mth.Add(5, 4))
+	fmt.Println("subtract 4 from 5 : subtraction is :", mth.Subtract(5, 4))
+
+}
--- main/main.go
+++ main/main.go
@@ -1,18 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"mth"
+)
 
 func init() {
 	fmt.Println("inside init")
-}
-
-func sum(a, b int) int {
-	return a + b
 }
 
 func main() {
 	fmt.Println("inside main")
 	fmt.Println("Hello, World")
 
-	fmt.Println("5 + 4 = ", sum(5, 4))
+	fmt.Println("5 + 4 = ", mth.Add(5, 4))
 }
